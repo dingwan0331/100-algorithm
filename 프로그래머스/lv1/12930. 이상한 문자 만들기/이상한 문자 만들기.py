90918604c8e6645a7e7eb15cb5c90c47cc4c3070
dingwan0331 <100751719+dingwan0331@users.noreply.github.com>
2022-08-30 19:32:12 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -1,24 +1,13 @@
-def solution(s):
-    answer = ''
-    j = 0
-    for i in s:
-        if i == ' ':
-            answer += ' '
-            j=0
-            continue
-        if j%2 ==0:
-            answer += i.upper()
-        else:
-            answer += i.lower()
-        j += 1
-    return answer
-    for i in range(len(s)):
-        if s[i]==' ':
-            answer += ' '
-            j=-1
-        elif j%2 == 0:
-            answer += s[i].upper()
-        else:
-            answer += s[i].lower()
-        j+=1
-    return answer
+# 문제 url = https://school.programmers.co.kr/learn/courses/30/lessons/12930?language=python3
+
+def solution(str):
+  answer = ''
+  count = 0
+  for i in str:
+    if i == ' ':
+      answer += ' '
+      count = 0
+      continue
+    answer += i.upper() if count % 2 == 0 else i.lower()
+    count += 1
+  return answer
