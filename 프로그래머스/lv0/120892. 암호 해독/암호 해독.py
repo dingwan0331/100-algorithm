5ad8a7ac7d7a34c4fc0524f9bc95142e38c56731
dingwan0331 <100751719+dingwan0331@users.noreply.github.com>
2022-11-13 18:19:04 +0900

    [level 0] Title: 암호 해독, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv0/120892. 암호 해독/암호 해독.py
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.py
@@ -1,13 +1,3 @@
 def solution(cipher, code):
-    N = len(cipher)
-    idx = code-1
-    idx_list = []
-    
-    while N > idx:
-        idx_list.append(idx)
-        idx += code
-        
-    answer = ''
-    for i in idx_list:
-        answer += cipher[i]
+    answer = cipher[code-1::code]
     return answer
