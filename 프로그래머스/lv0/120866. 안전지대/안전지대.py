064196a45a898a93d36219a318accbca61e91b6c
dingwan0331 <100751719+dingwan0331@users.noreply.github.com>
2022-12-09 17:04:22 +0900

    [level 0] Title: 안전지대, Time: 0.09 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120866. 안전지대/안전지대.py
+++ 프로그래머스/lv0/120866. 안전지대/안전지대.py
@@ -14,6 +14,7 @@ def solution(board):
                 continue
             answer[nx][ny] = 0
         return 
+    
     for i in range(N):
         for j in range(N):
             if board[i][j] == 1:
