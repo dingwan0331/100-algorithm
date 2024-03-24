50f24b44936c87fcb7f00af414938211987e7f5f
정진관 <100751719+dingwan0331@users.noreply.github.com>
2024-03-24 23:31:53 +0900

    Time: 42 ms (79.79%), Space: 16.8 MB (5.40%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -2,11 +2,11 @@ class Solution:
     def romanToInt(self, s: str) -> int:
         symbol_map = {"I":1,"X":10,"C":100,"M":1000,"V":5,"L":50,"D":500}
         answer = 0
-        previous = 0 
-        for i in s[::-1]:
-            if symbol_map[i] >= previous:
-                answer += symbol_map[i]
+        for i in range(len(s)):
+            if i == len(s)-1:
+                answer += symbol_map[s[i]]
+            elif symbol_map[s[i]] >= symbol_map[s[i+1]]:
+                answer += symbol_map[s[i]]
             else:
-                answer -= symbol_map[i]
-            previous = symbol_map[i]
+                answer -= symbol_map[s[i]]
         return answer
